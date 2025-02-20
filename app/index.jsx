c0f600d2f21b1814cc989b982708bcd4d3ba9221
Through2Dexter <162742084+Through2Dexter@users.noreply.github.com>
2025-02-20 02:54:19 +0000

    understanding tailwind & babel

--- app/index.jsx
+++ app/index.jsx
@@ -1,24 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { StatusBar } from "react-native-web";
 import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aodea!</Text>
-      <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: "blue" }}>
-        go to profile
-      </Link>
+    <View className="bg-blue-500 p-4">
+      <Text className="text-white">Hello, Tailwind!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
